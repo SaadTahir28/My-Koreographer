eef152918c8a4321bdc4ce693e59933189b9e95c
SaadTahir28 <saadtahir28@gmail.com>
2018-10-06 18:50:26 +0500

    Event system and Audio player
    Events are saved, deleted, loaded, added and delay can also be added when calling events

---
+++ Assets/AudioManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+
+    public AudioSource audioSource;
+    bool flag = false;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            audioSource.Play();
+            flag = true;
+
+        }
+        if (Input.GetKeyDown(KeyCode.O))
+        {
+            flag = false;
+            audioSource.Pause();
+        }
+
+        //if (flag)
+        //    Debug.Log(audioSource.time);
+
+
+
+    }
+
+
+
+}
